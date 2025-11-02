717009bd065a0cb5c018e588473688c7308abb99
huynhphuochien12 <hien221649@student.nctu.edu.vn>
2025-11-02 14:46:08 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://group14-projectbackend.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
 // Add token to all requests if exists
